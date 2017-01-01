17c46defb7a52f8f2f90ec7c893e4c6764a3e44c
Tokyodays <kikuchi@gmail.com>
2017-01-01 17:10:11 +0800

    update form

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -222,11 +222,45 @@ $(function(){
 		}
 	});
 
-	$('#requireform').submit(function(){
-		var $form = $('#requireform');
-		var query = $form.serialize();
-		console.log(query);
-	})
+	$('#requireform').submit(function(e){
+		e.preventDefault();
+
+		var $form = $(this);
+		var $button = $form.find('#submitbutton');
+
+    $.ajax({
+        url: $form.attr('action'),
+        type: $form.attr('method'),
+        data: $form.serialize()
+            + '&delay=1',  // （デモ用に入力値をちょいと操作します）
+        timeout: 10000,  // 単位はミリ秒
+
+        // 送信前
+        beforeSend: function(xhr, settings) {
+            // ボタンを無効化し、二重送信を防止
+            $button.attr('disabled', true);
+        },
+        // 応答後
+        complete: function(xhr, textStatus) {
+            // ボタンを有効化し、再送信を許可
+            $button.attr('disabled', false);
+        },
+
+        // 通信成功時の処理
+        success: function(result, textStatus, xhr) {
+            // 入力値を初期化
+            $form[0].reset();
+
+            $('#result').text('OK');
+        },
+
+        // 通信失敗時の処理
+        error: function(xhr, textStatus, error) {}
+    });
+
+
+	});
+
 /*
   $('.effect-left').onScreen({
     container: window,
