c025175e4d29eeca22274d2d4c0fa45bff7d1ef7
Tokyodays <kikuchi@gmail.com>
2018-06-23 18:15:45 +0900

    update server

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 
 var express = require('express');
 var app = express();
+var morgan      = require('morgan');
+var compression = require('compression');
+var serveStatic = require('serve-static');
+var basicAuth   = require('basic-auth-connect');
 //var router = app.Router();
 
 var user = process.env.USER;
@@ -9,12 +13,12 @@ var pass = process.env.PASS;
 app.set('port', process.env.PORT || 3000);
 
 if (user && pass) {
-  app.use(express.basicAuth(user, pass));
+  app.use(basicAuth(user, pass));
 }
 
-app.use(express.logger('dev'));
-app.use(express.compress());
-app.use(express.static(__dirname + '/dist'));
+app.use(morgan('dev'));
+app.use(compression());
+app.use(serveStatic(__dirname + '/dist'));
 //app.use('/static', express.static(__dirname + '/dist/public'));
 
 app.listen(app.get('port'), function() {
